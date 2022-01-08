8a86c87d26e5b83474048d499ffd5aa252ae11b0
Asa Shepard <78510770+asashepard@users.noreply.github.com>
2022-01-08 12:10:58 -0600

    Create PlayerGUI.java

---
+++ src/main/java/com/gmail/creepycucumber1/hungerclans/gui/PlayerGUI.java
@@ -0,0 +1,69 @@
+package com.gmail.creepycucumber1.hungerclans.gui;
+
+import com.gmail.creepycucumber1.hungerclans.HungerClans;
+import com.gmail.creepycucumber1.hungerclans.util.ItemUtil;
+import com.gmail.creepycucumber1.hungerclans.util.TextUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class PlayerGUI extends GUI {
+    private Player player;
+
+    public PlayerGUI(HungerClans plugin, Player player, String uuid) {
+        super(plugin, player.getUniqueId(), "Clan Member", 1);
+        this.player = player;
+
+        ItemStack skull = getSkull(uuid);
+        items[4] = new GUIItem(skull, "skull");
+
+        ItemStack grayPane = ItemUtil.createItemStack(Material.GRAY_STAINED_GLASS_PANE, "");
+        GUIItem gray = new GUIItem(grayPane, "gray");
+        items[3] = gray; items[5] = gray;
+
+        ItemStack green = ItemUtil.createItemStack(Material.GREEN_CONCRETE, "&2Promote",
+                "&7if leader promotes trusted to leader,",
+                "&7leader is demoted");
+        items[1] = new GUIItem(green, uuid);
+
+        ItemStack red = ItemUtil.createItemStack(Material.RED_CONCRETE, "&4Demote/Kick");
+        items[7] = new GUIItem(red, uuid);
+
+
+    }
+
+    public ItemStack getSkull(String uuid) {
+        ItemStack skull = new ItemStack(Material.PLAYER_HEAD, 1);
+        SkullMeta skullMeta = (SkullMeta) skull.getItemMeta();
+        skullMeta.setOwningPlayer(player);
+        skullMeta.setDisplayName(TextUtil.convertColor("&2" + Bukkit.getOfflinePlayer(UUID.fromString(uuid)).getName() + " &7[" +
+                plugin.getClanManager().getRole(Bukkit.getOfflinePlayer(UUID.fromString(uuid))) + "]"));
+        skull.setItemMeta(skullMeta);
+        return skull;
+    }
+
+    @Override
+    public void open() {
+        player.openInventory(createInventory());
+    }
+
+    @Override
+    public void clicked(Player p, GUIItem item) {
+        if(item.getItem().getType().equals(Material.GREEN_CONCRETE)) {
+            Bukkit.getServer().dispatchCommand(p, "c promote " + Bukkit.getOfflinePlayer(UUID.fromString(item.getId())).getName());
+            p.closeInventory();
+        } else if(item.getItem().getType().equals(Material.RED_CONCRETE)) {
+            Bukkit.getServer().dispatchCommand(p, "c demote " + Bukkit.getOfflinePlayer(UUID.fromString(item.getId())).getName());
+            p.closeInventory();
+        }
+    }
+
+}
