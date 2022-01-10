4c97e2d56441bfeab51492b8b3db1d23e0c7f211
Asa Shepard <78510770+asashepard@users.noreply.github.com>
2022-01-09 22:10:13 -0600

    Update ClanDashboardGUI.java

--- src/main/java/com/gmail/creepycucumber1/hungerclans/gui/ClanDashboardGUI.java
+++ src/main/java/com/gmail/creepycucumber1/hungerclans/gui/ClanDashboardGUI.java
@@ -4,39 +4,44 @@ import com.gmail.creepycucumber1.hungerclans.HungerClans;
 import com.gmail.creepycucumber1.hungerclans.util.ColorUtil;
 import com.gmail.creepycucumber1.hungerclans.util.ItemUtil;
 import com.gmail.creepycucumber1.hungerclans.util.TextUtil;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.OfflinePlayer;
+import org.bukkit.*;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
+import org.checkerframework.checker.units.qual.A;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class ClanDashboardGUI extends GUI {
     private Player player;
 
     public ClanDashboardGUI(HungerClans plugin, Player player) {
-        super(plugin, player.getUniqueId(), "&lClan Dashboard", 4);
+        super(plugin, player.getUniqueId(), ColorUtil.colorToStringCode(plugin.getClanManager().getColor(plugin.getClanManager().getClan(player))) + "&lClan Dashboard", 4);
         this.player = player;
         String clanName = plugin.getClanManager().getClan(player);
         ChatColor color = plugin.getClanManager().getColor(clanName);
+        String colorCode = ColorUtil.colorToStringCode(color);
         String role = plugin.getClanManager().getRole(player);
 
-        ItemStack banner = ItemUtil.createItemStack(plugin.getClanManager().getBanner(clanName).getType(), color + "&l" + clanName);
+        ItemStack banner = plugin.getClanManager().getBanner(clanName);
+        ItemMeta bannerMeta = banner.getItemMeta();
+        bannerMeta.setDisplayName(TextUtil.convertColor(colorCode + "&l" + clanName + " &r" + colorCode + "[" + plugin.getClanManager().getCode(clanName) + "]"));
+        ArrayList<String> bannerLore = new ArrayList<>();
+        if(plugin.getClanManager().getMotto(clanName).length() > 0)
+            bannerLore.add(TextUtil.convertColor("&f&o\"" + plugin.getClanManager().getMotto(clanName) + "\""));
+        bannerMeta.setLore(bannerLore);
+        banner.setItemMeta(bannerMeta);
         items[13] = new GUIItem(banner, "banner");
 
-        ItemStack grayPane = ItemUtil.createItemStack(Material.GRAY_STAINED_GLASS_PANE, "");
-        GUIItem gray = new GUIItem(grayPane, "gray");
-        items[4] = gray; items[11] = gray; items[12] = gray; items[14] = gray; items[15] = gray; items[22] = gray;
-
         ItemStack viewInfo = ItemUtil.createItemStack(Material.PAPER, "&7Members: &f" + plugin.getClanManager().getMembers(clanName).size(),
+                "&7Points: &e" + plugin.getClanManager().getPoints(clanName),
                 "&7Color: " + color + ColorUtil.colorToString(color),
                 "&7Created: &f" + plugin.getClanManager().getCreated(clanName));
-        ItemStack changeColor = ItemUtil.createItemStack(Material.PAINTING, "&6Change &3Clan &2Color");
+        ItemStack changeColor = ItemUtil.createItemStack(Material.PAINTING, TextUtil.toRainbow("Change Clan Color"));
         if(role.equalsIgnoreCase("trusted") || role.equalsIgnoreCase("leader")) {
             items[10] = new GUIItem(changeColor, "color");
             items[0] = new GUIItem(viewInfo, "info");
@@ -44,21 +49,34 @@ public class ClanDashboardGUI extends GUI {
             items[10] = new GUIItem(viewInfo, "info");
         }
 
-        ItemStack help = ItemUtil.createItemStack(Material.COMPASS, "&7Command Help");
+        ItemStack help = ItemUtil.createItemStack(Material.COMPASS, "&7Help");
         items[8] = new GUIItem(help, "help");
 
         ItemStack viewClans = ItemUtil.createItemStack(Material.SPYGLASS, "&7View All Clans");
         items[16] = new GUIItem(viewClans, "clans");
 
-        int firstSlot = (plugin.getClanManager().getMembers(clanName).size() > 9 ? 18 : 27);
         for(int i = 0; i < plugin.getClanManager().getMembers(clanName).size(); i++) {
-            boolean jump = firstSlot == 18 && i > 4;
             String uuid = plugin.getClanManager().getMembers(clanName).get(i);
             ItemStack skull = getSkull(uuid);
-            int slot = firstSlot + i;
-            if(jump) slot++;
+            int slot = 27 + i;
             items[slot] = new GUIItem(skull, uuid);
         }
+
+        ItemStack grayPane = ItemUtil.createItemStack(Material.GRAY_STAINED_GLASS_PANE, " ");
+        GUIItem gray = new GUIItem(grayPane, "pane");
+        for(int i = 0; i <= 26; i += 2)
+            if(items[i] == null)
+                items[i] = gray;
+
+        ItemStack whitePane = ItemUtil.createItemStack(Material.WHITE_STAINED_GLASS_PANE, " ");
+        GUIItem white = new GUIItem(whitePane, "pane");
+        for(int i = 1; i <= 26; i += 2)
+            if(i != 13)
+                items[i] = white;
+
+        ItemStack blackPane = ItemUtil.createItemStack(Material.BLACK_STAINED_GLASS_PANE, " ");
+        GUIItem black = new GUIItem(blackPane, "pane");
+        items[4] = black; items[12] = black; items[14] = black; items[22] = black;
 
     }
 
@@ -68,6 +86,9 @@ public class ClanDashboardGUI extends GUI {
         skullMeta.setOwningPlayer(player);
         skullMeta.setDisplayName(TextUtil.convertColor("&2" + Bukkit.getOfflinePlayer(UUID.fromString(uuid)).getName() + " &7[" +
                 plugin.getClanManager().getRole(Bukkit.getOfflinePlayer(UUID.fromString(uuid))) + "]"));
+        ArrayList<String> lore = new ArrayList<>();
+        lore.add(TextUtil.convertColor("&7Joined: &f" + plugin.getPlayerManager().getJoinedClan(Bukkit.getOfflinePlayer(UUID.fromString(uuid)))));
+        skullMeta.setLore(lore);
         skull.setItemMeta(skullMeta);
         return skull;
     }
@@ -79,7 +100,6 @@ public class ClanDashboardGUI extends GUI {
 
     @Override
     public void clicked(Player p, GUIItem item) {
-        String clanName = plugin.getClanManager().getClan(p);
         if(item.getItemId().equalsIgnoreCase("color")) {
             p.closeInventory();
             plugin.getGUIManager().openGUI(p, new ClanColorGUI(plugin, p));
@@ -93,7 +113,8 @@ public class ClanDashboardGUI extends GUI {
             plugin.getServer().dispatchCommand(p, "c help");
         }
         else if(item.getItemId().equalsIgnoreCase("info") ||
-                item.getItemId().equalsIgnoreCase("gray")) {
+                item.getItemId().equalsIgnoreCase("pane") ||
+                item.getItemId().equalsIgnoreCase("banner")) {
             // do nothing
         }
         else { //skull
