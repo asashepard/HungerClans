830017df6245f29cca413acd3c65c58ca3aa98eb
Asa Shepard <78510770+asashepard@users.noreply.github.com>
2022-01-09 22:16:50 -0600

    Update TextUtil.java

--- src/main/java/com/gmail/creepycucumber1/hungerclans/util/TextUtil.java
+++ src/main/java/com/gmail/creepycucumber1/hungerclans/util/TextUtil.java
@@ -12,10 +12,45 @@ import net.md_5.bungee.api.ChatColor;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class TextUtil {
     public static String convertColor(String s){
         return ChatColor.translateAlternateColorCodes('&', s);
+    }
+
+    public static String toRainbow(String s) {
+        String result = "";
+
+        java.util.List<String> codes = List.of("&4", "&c", "&6", "&e", "&2", "&a", "&b", "&3", "&9", "&d", "&5");
+        for(int i = 0; i < s.length(); i++) {
+            if(s.substring(i, i + 1) != " ")
+                result += codes.get(i % codes.toArray().length);
+            result += s.substring(i, i + 1);
+        }
+
+        return ChatColor.translateAlternateColorCodes('&', result);
+    }
+
+    public static String getSuffix(String n) {
+        String suffix = "th";
+        if(n.length() > 1) {
+            if(n.substring(1).equalsIgnoreCase("1") && !n.substring(0, 1).equalsIgnoreCase("1"))
+                suffix = "st";
+            else if(n.substring(1).equalsIgnoreCase("2") && !n.substring(0, 1).equalsIgnoreCase("1"))
+                suffix = "nd";
+            else if(n.substring(1).equalsIgnoreCase("3") && !n.substring(0, 1).equalsIgnoreCase("1"))
+                suffix = "th";
+        }
+        else {
+            if(n.equalsIgnoreCase("1"))
+                suffix = "st";
+            else if(n.equalsIgnoreCase("2"))
+                suffix = "nd";
+            else if(n.equalsIgnoreCase("3"))
+                suffix = "th";
+        }
+        return suffix;
     }
 
     public static String convertArray(Object[] args){
@@ -32,37 +67,5 @@ public class TextUtil {
         }
 
         return sb.substring(0, sb.length()-1);
-    }
-
-    public static Collection<ChatColor> getGradients(String text, ChatColor color1, ChatColor color2){
-        text = text.replaceAll("&k", "");
-        text = text.replaceAll("&l", "");
-        text = text.replaceAll("&m", "");
-        text = text.replaceAll("&n", "");
-        text = text.replaceAll("&o", "");
-
-        int length = text.length();
-        double rStep = Math.abs((double) (color1.getColor().getRed() - color2.getColor().getRed()) / length);
-        double gStep = Math.abs((double) (color1.getColor().getGreen() - color2.getColor().getGreen()) / length);
-        double bStep = Math.abs((double) (color1.getColor().getBlue() - color2.getColor().getBlue()) / length);
-        if (color1.getColor().getRed() > color2.getColor().getRed()) rStep = -rStep;
-        if (color1.getColor().getGreen() > color2.getColor().getGreen()) gStep = -gStep;
-        if (color1.getColor().getBlue() > color2.getColor().getBlue()) bStep = -bStep;
-
-        Color currentColor = color1.getColor();
-        ArrayList<ChatColor> colors = new ArrayList<>();
-        for(int i = 0; i < length-1; i++){
-            int red = (int) Math.round(currentColor.getRed() + rStep);
-            int green = (int) Math.round(currentColor.getGreen() + gStep);
-            int blue = (int) Math.round(currentColor.getBlue() + bStep);
-            if (red > 255) red = 255; if (red < 0) red = 0;
-            if (green > 255) green = 255; if (green < 0) green = 0;
-            if (blue > 255) blue = 255; if (blue < 0) blue = 0;
-
-            currentColor = new Color(red, green, blue);
-            colors.add(ChatColor.of(currentColor));
-        }
-
-        return colors;
     }
 }
