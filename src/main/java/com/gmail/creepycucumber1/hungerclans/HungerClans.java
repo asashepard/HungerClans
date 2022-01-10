621e555e3b4a3c5c6323afe71c416456c8eb9ff6
Asa Shepard <78510770+asashepard@users.noreply.github.com>
2022-01-09 22:03:02 -0600

    Update HungerClans.java

--- src/main/java/com/gmail/creepycucumber1/hungerclans/HungerClans.java
+++ src/main/java/com/gmail/creepycucumber1/hungerclans/HungerClans.java
@@ -1,68 +1,104 @@
 package com.gmail.creepycucumber1.hungerclans;
 
 import com.gmail.creepycucumber1.hungerclans.clan.ClanManager;
+import com.gmail.creepycucumber1.hungerclans.clan.PlayerManager;
+import com.gmail.creepycucumber1.hungerclans.clan.WarManager;
 import com.gmail.creepycucumber1.hungerclans.command.ClanCommand;
+import com.gmail.creepycucumber1.hungerclans.command.ClanWhisperCommand;
 import com.gmail.creepycucumber1.hungerclans.command.CommandBase;
+import com.gmail.creepycucumber1.hungerclans.command.WarCommand;
 import com.gmail.creepycucumber1.hungerclans.data.DataManager;
 import com.gmail.creepycucumber1.hungerclans.event.EventManager;
 import com.gmail.creepycucumber1.hungerclans.gui.GUIManager;
-import com.gmail.creepycucumber1.hungerclans.nametag.NametagManager;
+import com.gmail.creepycucumber1.hungerclans.runnable.NametagManager;
+import com.gmail.creepycucumber1.hungerclans.runnable.Teleport;
+import com.gmail.creepycucumber1.hungerclans.runnable.WarMonitor;
 import com.gmail.creepycucumber1.hungerclans.util.TextUtil;
+import net.ess3.api.IEssentials;
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
-import org.bukkit.NamespacedKey;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.serialization.ConfigurationSerialization;
-import org.bukkit.enchantments.Enchantment;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.checkerframework.checker.units.qual.A;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Calendar;
+import java.util.UUID;
 
 public final class HungerClans extends JavaPlugin {
 
+    private IEssentials ess;
+    private Economy vault;
     private ArrayList<CommandBase> commands;
     private ClanManager clanManager;
+    private WarManager warManager;
+    private PlayerManager playerManager;
     private DataManager dataManager;
     private GUIManager guiManager;
     private NametagManager nametagManager;
+    private WarMonitor warMonitor;
+    private Teleport teleport;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        commands = registerCommands();
+        commands = new ArrayList<>(registerCommands());
 
         clanManager = new ClanManager(this);
+        warManager = new WarManager(this);
+        playerManager = new PlayerManager(this);
         dataManager = new DataManager(this);
         guiManager = new GUIManager(this);
 
         nametagManager = new NametagManager(this);
+        warMonitor = new WarMonitor(this);
+        teleport = new Teleport(this);
         nametagManager.nametag();
+        warMonitor.monitorWars();
+
+        try{
+            ess = (IEssentials) getServer().getPluginManager().getPlugin("Essentials");
+
+            if(ess==null){
+                getLogger().warning("Failed to hook in with Essentials!");
+            }
+        }
+        catch(Exception ex){
+            getLogger().warning("Failed to hook in with Essentials!");
+        }
+
+        if(getServer().getPluginManager().getPlugin("Vault") != null) {
+            RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+            if(rsp != null){
+                vault = rsp.getProvider();
+            }
+        }
 
         PluginManager pm = Bukkit.getServer().getPluginManager();
         pm.registerEvents(new EventManager(this), this);
+
+        this.rewardClanMembers();
 
         getLogger().info("HungerClans has started.");
     }
 
     @Override
     public void onDisable() {
-        //.
+        getLogger().info("HungerClans has been disabled.");
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
-        for(CommandBase c : commands){
-            if(c.getCommand().equalsIgnoreCase(label)|| (c.getAliases()!=null&&c.getAliases().contains(label))){
+        for(CommandBase c : commands)
+            if(c.getCommand().equalsIgnoreCase(label) || (c.getAliases() != null && c.getAliases().contains(label))) {
                 boolean result = c.execute(sender, args);
-                if(!result){
+                if(!result)
                     sender.sendMessage(TextUtil.convertColor("&cInvalid command usage!\n&7/" + label + " " + c.getUsage()));
-                }
             }
-        }
 
         return true;
     }
@@ -70,17 +106,49 @@ public final class HungerClans extends JavaPlugin {
     private ArrayList<CommandBase> registerCommands() {
         ArrayList<CommandBase> commands = new ArrayList<>();
         commands.add(new ClanCommand(this));
+        commands.add(new ClanWhisperCommand(this));
+        commands.add(new WarCommand(this));
         return commands;
     }
 
+    private void rewardClanMembers() {
+        int day = Calendar.getInstance().getTime().getDay();
+        if(day % 3 != 0) return;
+        ConfigurationSection cfg = getDataManager().getConfig().getConfigurationSection("clans");
+        for(String clan : cfg.getKeys(false)) {
+            for(String uuid : cfg.getStringList(clan + ".members")) {
+                OfflinePlayer player = Bukkit.getOfflinePlayer(UUID.fromString(uuid));
+                getVault().depositPlayer(player, getClanManager().getPoints(clan));
+                getEssentials().getUser(UUID.fromString(uuid)).addMail("You have received $" + getClanManager().getPoints(clan) +
+                        " for being a member of " + getClanManager().getClan(player) + "!");
+                getClanManager().addPoints(clan, (int) (Math.random() * 7.5 + 1));
+            }
+        }
+    }
+
+    public IEssentials getEssentials() {
+        return ess;
+    }
+    public Economy getVault() {
+        return vault;
+    }
     public ClanManager getClanManager() {
         return clanManager;
+    }
+    public WarManager getWarManager() {
+        return warManager;
+    }
+    public PlayerManager getPlayerManager() {
+        return playerManager;
     }
     public DataManager getDataManager() {
         return dataManager;
     }
     public GUIManager getGUIManager() {
         return guiManager;
+    }
+    public Teleport getTeleporter() {
+        return teleport;
     }
 
 }
