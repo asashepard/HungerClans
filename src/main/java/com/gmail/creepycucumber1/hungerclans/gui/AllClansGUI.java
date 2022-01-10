42ca35217adc2074e6d4f66ff445255a6a0f0c09
Asa Shepard <78510770+asashepard@users.noreply.github.com>
2022-01-09 22:09:11 -0600

    Update AllClansGUI.java

--- src/main/java/com/gmail/creepycucumber1/hungerclans/gui/AllClansGUI.java
+++ src/main/java/com/gmail/creepycucumber1/hungerclans/gui/AllClansGUI.java
@@ -2,13 +2,11 @@ package com.gmail.creepycucumber1.hungerclans.gui;
 
 import com.gmail.creepycucumber1.hungerclans.HungerClans;
 import com.gmail.creepycucumber1.hungerclans.util.ColorUtil;
-import com.gmail.creepycucumber1.hungerclans.util.ItemUtil;
 import com.gmail.creepycucumber1.hungerclans.util.TextUtil;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
 
@@ -16,18 +14,27 @@ public class AllClansGUI extends GUI {
     private Player player;
 
     public AllClansGUI(HungerClans plugin, Player player) {
-        super(plugin, player.getUniqueId(), "Clan Explorer", 2);
+        super(plugin, player.getUniqueId(), "Clan Explorer", (plugin.getClanManager().getClanList().size() > 9 ? 2 : 1));
         this.player = player;
 
         ConfigurationSection cfg = plugin.getDataManager().getConfig().getConfigurationSection("clans");
         ArrayList<String> clansList = new ArrayList<>(cfg.getKeys(false));
         for(int i = 0; i < clansList.size(); i++) {
             String clanName = clansList.get(i);
-            ItemStack clan = ItemUtil.createItemStack(plugin.getClanManager().getBanner(clanName).getType(),
-                    plugin.getClanManager().getColor(clanName) + clanName,
-                    "&7Members: &f" + plugin.getClanManager().getMembers(clanName).size(),
-                    "&7Members: &f" + plugin.getClanManager().getCreated(clanName));
-            items[i] = new GUIItem(clan, clanName);
+            ItemStack banner = plugin.getClanManager().getBanner(clanName);
+            ItemMeta bannerMeta = banner.getItemMeta();
+            bannerMeta.setDisplayName(TextUtil.convertColor(ColorUtil.colorToStringCode(plugin.getClanManager().getColor(clanName)) + "&l" + clanName +
+                    "&r" + ColorUtil.colorToStringCode(plugin.getClanManager().getColor(clanName)) +
+                    " [" + plugin.getClanManager().getCode(clanName) + "]"));
+            ArrayList<String> bannerLore = new ArrayList<>();
+            bannerLore.add(TextUtil.convertColor("&7 - Points: &e" + plugin.getClanManager().getPoints(clanName)));
+            bannerLore.add(TextUtil.convertColor("&7 - Members: &f" + plugin.getClanManager().getMembers(clanName).size()));
+            bannerLore.add(TextUtil.convertColor("&7 - Created: &f" + plugin.getClanManager().getCreated(clanName)));
+            if(plugin.getClanManager().getMotto(clanName).length() > 0)
+                bannerLore.add(TextUtil.convertColor("&f&o\"" + plugin.getClanManager().getMotto(clanName) + "\""));
+            bannerMeta.setLore(bannerLore);
+            banner.setItemMeta(bannerMeta);
+            items[i] = new GUIItem(banner, clanName);
         }
     }
 
@@ -41,8 +48,9 @@ public class AllClansGUI extends GUI {
         p.closeInventory();
         if(item.getItemId().equalsIgnoreCase(plugin.getClanManager().getClan(p))) {
             plugin.getGUIManager().openGUI(p, new ClanDashboardGUI(plugin, p));
+        } else {
+            plugin.getGUIManager().openGUI(p, new OtherClanGUI(plugin, p, item.getItemId()));
         }
-        plugin.getGUIManager().openGUI(p, new OtherClanGUI(plugin, p, item.getItemId()));
     }
 
 }
