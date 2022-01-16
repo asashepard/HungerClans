4765ce732d54b5c4d174ba34c3bfc399d6a47af4
Asa Shepard <78510770+asashepard@users.noreply.github.com>
2022-01-15 20:27:42 -0600

    Add files via upload

---
+++ src/main/java/com/gmail/creepycucumber1/hungerclans/data/ConfigManager.java
@@ -0,0 +1,68 @@
+package com.gmail.creepycucumber1.hungerclans.data;
+
+import com.gmail.creepycucumber1.hungerclans.HungerClans;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+
+public class ConfigManager {
+
+    private static final String CONFIG_NAME = "config.yml";
+    private HungerClans plugin;
+    private FileConfiguration config;
+    private File configFile;
+
+    public ConfigManager(HungerClans plugin){
+        this.plugin = plugin;
+        saveDefaultConfig();
+        reloadConfig();
+    }
+
+    public void reloadConfig(){
+        if(this.configFile == null){
+            this.configFile = new File(this.plugin.getDataFolder(), CONFIG_NAME);
+        }
+
+        config = YamlConfiguration.loadConfiguration(configFile);
+
+        InputStream defaultStream = plugin.getResource(CONFIG_NAME);
+
+        if(defaultStream!=null){
+            YamlConfiguration defaultConfig = YamlConfiguration.loadConfiguration(new InputStreamReader(defaultStream));
+            config.setDefaults(defaultConfig);
+        }
+    }
+
+    public FileConfiguration getConfig(){
+        if(config==null) reloadConfig();
+
+        return config;
+    }
+
+    public void saveConfig(){
+        if(config == null || configFile == null) return;
+
+        try{
+            getConfig().save(configFile);
+        }
+        catch(IOException ex){
+            plugin.getLogger().log(Level.SEVERE, "Could not save to config " + configFile, ex);
+        }
+    }
+
+    public void saveDefaultConfig(){
+        if(configFile==null){
+            configFile = new File(plugin.getDataFolder(), CONFIG_NAME);
+        }
+
+        if(!configFile.exists()){
+            plugin.saveResource(CONFIG_NAME, false);
+        }
+    }
+
+}
